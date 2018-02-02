1b10f1b5e0e8c5809e8f3a87708c49cd37a013b6
Janne Paalijarvi <jpaalija@gmail.com>
2018-02-02 16:35:04 +0200

    Renamed files to cc

--- src/research/listupgrades.cc
+++ src/research/listupgrades.cc
@@ -7,21 +7,22 @@
 
 int main()
 {
-  pkgInitConfig(*_config);
-  pkgInitSystem(*_config, _system);
-
   pkgCache* pkg_cache = NULL;
   pkgDepCache* dep_cache = NULL;
+  pkgDepCache::StateCache* package_state = NULL;
   pkgCacheFile cache_file;
+
+  pkgInitConfig(*_config);
+  pkgInitSystem(*_config, _system);
 
   pkg_cache = cache_file.GetPkgCache();
   dep_cache = cache_file.GetDepCache();
 
   for (pkgCache::PkgIterator package = pkg_cache->PkgBegin(); !package.end(); package++)
   {
-    pkgDepCache::StateCache &state = (*dep_cache)[package];
-    
-    if(package.CurrentVer() && state.Upgradable())
+    package_state = &((*dep_cache)[package]);
+
+    if(package.CurrentVer() && package_state->Upgradable())
     {
       std::cout << package.Name() << std::endl;
     }
