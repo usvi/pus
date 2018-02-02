4e08b42abc1521d91deddf544aee194b6d2b2de9
Janne Paalijarvi <jpaalija@gmail.com>
2018-02-02 15:39:04 +0200

    Typo fix

--- src/research/listupgrades.c
+++ src/research/listupgrades.c
@@ -1,7 +1,9 @@
-// Adapted from rom:
+// Adapted from from:
 // https://stackoverflow.com/questions/341520/how-to-use-libapt-or-libept-in-debian-like-system-to-list-packages-and-get-the?noredirect=1
 
+
 #include <apt-pkg/cachefile.h>
+
 
 int main()
 {
